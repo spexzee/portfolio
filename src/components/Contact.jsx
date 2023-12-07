1a246b2075de079a424814ebd33ddfb415c0202f
spexzee <abub5979@gmail.com>
2023-12-07 16:40:27 +0530

    change btn color

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -118,7 +118,7 @@ const Contact = () => {
 
           <button
             type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            className='bg-[#294a7e] py-3 px-8 rounded-xl outline-none w-100 text-white font-bold shadow-md shadow-primary'
           >
             {loading ? "Sending..." : "Send"}
           </button>
