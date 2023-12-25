1725f092db7a1efa997271719c20b7bcd8d9da3d
Spexzee_Abu <104344646+spexzee@users.noreply.github.com>
2023-12-25 14:24:33 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,8 +22,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Spexzee</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a Front-End Developer,Developed user <br className='sm:block hidden' />
-            interfaces and web applications
+            I am a Front-End React Developer, from <br className='sm:block hidden' />
+            Bengaluru.
           </p>
           <button class="button-resume mt-20 bg-violet-500" role="button" onClick={handleClickResume}><span class="text">Resume</span><span></span></button>
         </div>
