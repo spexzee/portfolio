638bac3e29439062c0f81b1486bdd007c527e2be
Abubakara Nadafa <104344646+spexzee@users.noreply.github.com>
2025-05-02 10:45:07 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -121,32 +121,6 @@ const technologies = [
 
 const projects = [
   {
-    name: "Fuzzie",
-    description:
-      "Fuzzie is a Next.js SaaS Project. Autmated work like sending messages to slack and Discord channels using Fuzzie.  (Still Working)",
-    tags: [
-      {
-        name: "next.js",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "clerk-Authentication",
-        color: "green-text-gradient",
-      },
-      {
-        name: "prisma",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "shadcn-ui & tailwind css",
-        color: "blue-text-gradient",
-      },
-    ],
-    image: fuzzie,
-    source_code_link: "https://github.com/spexzee/sp-fuzzie",
-    demo_link: "https://sp-fuzzie.vercel.app/",
-  },
-  {
     name: "Youtube-Player",
     description:
       "Developed a YouTube Player in React.js for easy video watching.Added a search tool to find related videos quickly.",
@@ -193,32 +167,6 @@ const projects = [
     image: promptWorld,
     source_code_link: "https://github.com/spexzee/prompt-world",
     demo_link: "https://spexzee-prompt.netlify.app/",
-  },
-  {
-    name: "iNote-Book",
-    description:
-      "Implemented note functionalities like creation, editing, and deletion, with MongoDB for data storage, resulting in a user - friendly application",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "mongodb",
-        color: "green-text-gradient",
-      },
-      {
-        name: "node",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "express",
-        color: "green-text-gradient",
-      },
-    ],
-    image: iNote,
-    source_code_link: "https://github.com/spexzee/iNotebook",
-    demo_link: "https://spexzee-notebook.netlify.app/",
   },
   {
     name: "GPT-3 Overview",
