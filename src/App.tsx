7749ea7d3a60f45e8b27a323befd7491c8756e1b
spexzee <abub5979@gmail.com>
2025-12-27 17:49:52 +0530

    manually failed build

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Hero, Navbar, Tech, Works, StarsCanvas, Experience, ErrorBoundary, ErrorTestButton } from "./components";
 import Footer from "./components/Footer";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient()
 
